11d5b84bbddcaf93746be6cbfe1958f0013500d9
Anuraj Jit Saikia <24f2008153@ds.study.iitm.ac.in>
2025-08-12 17:36:06 +0530

    The Side roads are made

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
 
 animate();
 
@@ -11,5 +12,6 @@ function animate() {
 
   canvas.height = window.innerHeight;
   car.draw(ctx);
+  road.draw(ctx);
   requestAnimationFrame(animate);
 }
--- roads.js
+++ roads.js
@@ -15,5 +15,15 @@ class Road {
   draw(ctx) {
     ctx.lineWidth = 5;
     ctx.strokeStyle = "white";
+
+    ctx.beginPath();
+    ctx.moveTo(this.left, this.top);
+    ctx.lineTo(this.left, this.bottom);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(this.right, this.top);
+    ctx.lineTo(this.right, this.bottom);
+    ctx.stroke();
   }
 }
