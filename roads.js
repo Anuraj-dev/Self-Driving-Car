b8fa2e6106a7fa604872a292c72c3600d0257f6f
Anuraj Jit Saikia <24f2008153@ds.study.iitm.ac.in>
2025-08-12 18:52:34 +0530

    Border modified

--- roads.js
+++ roads.js
@@ -10,6 +10,17 @@ class Road {
     const infinity = 1000000;
     this.top = -infinity;
     this.bottom = infinity;
+
+    const topLeft = { x: this.left, y: this.top };
+    const topRight = { x: this.right, y: this.top };
+    const bottomLeft = { x: this.left, y: this.bottom };
+    const bottomRight = { x: this.right, y: this.bottom };
+    this.borders = [
+      [topLeft, bottomLeft], //?Why we are using arrays and borders .... what do they really do?
+      [topRight, bottomRight],
+    ];
+
+    //TODO: Make curv roads ... this is your homework
   }
 
   getLaneCenter(laneIndex) {
@@ -25,18 +36,22 @@ class Road {
     ctx.lineWidth = 5;
     ctx.strokeStyle = "white";
 
-    for (let i = 0; i <= this.laneCount; i++) {
+    for (let i = 1; i <= this.laneCount - 1; i++) {
       const x = lerp(this.left, this.right, i / this.laneCount); //*How does this working
 
-      if (i > 0 && i < this.laneCount) {
-        ctx.setLineDash([20, 20]);
-      } else {
-        ctx.setLineDash([]);
-      }
+      ctx.setLineDash([20, 20]);
       ctx.beginPath();
       ctx.moveTo(x, this.top);
       ctx.lineTo(x, this.bottom);
       ctx.stroke();
     }
+
+    ctx.setLineDash([]);
+    this.borders.forEach((border) => {
+      ctx.beginPath();
+      ctx.moveTo(border[0].x, border[0].y);
+      ctx.lineTo(border[1].x, border[1].y);
+      ctx.stroke();
+    });
   }
 }
